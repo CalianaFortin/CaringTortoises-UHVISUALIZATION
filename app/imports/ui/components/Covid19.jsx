788083d20ed35dda264a1dbd5cca3e2d3952033d
buccatm <70668682+buccatm@users.noreply.github.com>
2020-11-09 20:06:42 -1000

    The map is now displayed. I still need to fix the zoom, but do not put merge into master just yet.

--- app/imports/ui/components/Covid19.jsx
+++ app/imports/ui/components/Covid19.jsx
@@ -1,1 +1,24 @@
 /* PLAN TO ADD STUFF HERE */
+import React from 'react';
+import L from 'leaflet';
+import datas from '../../../data/campusmap.json';
+import 'leaflet/dist/leaflet.css';
+
+export class Covid19 extends React.Component {
+  state = {};
+
+  componentDidMount() {
+    console.log(datas);
+    const map = L.map('map').setView([0 ,0],1);
+    L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=XC0B5O0zJTg9gUhUn68K', {
+      attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> ' +
+          '<a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>' }).addTo(map);
+    L.geoJSON(datas).addTo(map);
+
+  }
+
+  render() {
+    return (<div id='map'></div>);
+  }
+
+}
