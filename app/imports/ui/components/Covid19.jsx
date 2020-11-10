c506e0f66f61e805bcf392306310fb12bc43e5d0
CalianaFortin <70739998+CalianaFortin@users.noreply.github.com>
2020-11-10 01:08:59 -1000

    Fixed the zoom
    Also added a few files I plan to change the legend to the slider

--- app/imports/ui/components/Covid19.jsx
+++ app/imports/ui/components/Covid19.jsx
@@ -1,4 +1,3 @@
-/* PLAN TO ADD STUFF HERE */
 import React from 'react';
 import L from 'leaflet';
 import datas from '../../../data/campusmap.json';
@@ -9,7 +8,7 @@ export class Covid19 extends React.Component {
 
   componentDidMount() {
     console.log(datas);
-    const map = L.map('map').setView([0 ,0],1);
+    const map = L.map('map').setView([21.29930,-157.81563], 16);
     L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=XC0B5O0zJTg9gUhUn68K', {
       attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> ' +
           '<a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>' }).addTo(map);
